7c3a4bbccf45939ad2585a5d75882c3387e91d0f
UbaydulloH00 <turonarsloni00@gmail.com>
2023-10-27 18:59:48 +0500

    end

---
+++ index.js
@@ -0,0 +1,109 @@
+// 1 chi misol
+// let arr =[1,2,3,4,5,6,7,8,9,11,13,15,17];
+// function newArray(arr){
+    
+//     let arr1 = arr.filter(function toq(value) {
+//        return value % 2 == 1
+//     })
+// console.log(arr1);
+
+// }
+
+// 2 chi masala
+
+
+
+//
+
+// let arr =[1,2,3,4,5,6,7,8,9,11,13,15,17];
+// function newArray(arr){
+//     let arr1 = [];
+//   for (i = 0 ; i<=arr.length; i++){
+//       if(arr[i] % 2 == 1){
+//       arr1.push(arr[i]**2);
+//       }
+//       if (arr[i] %2 == 0 ){
+//         arr1.push(arr[i]**3);
+//       }
+//   }
+// console.log(arr1);
+
+// }
+// newArray(arr);
+
+
+// let arr =[1,2,3,4,5,6,7,8,9,11,13,15,17];
+// function newArray(arr){
+//     let arr1 = [];
+//   for (i = 0 ; i<=arr.length; i++){
+//       if(arr[i] % 2 == 1){
+//         arr1 = arr.map(function(n){
+//             return n ** 2
+//         })
+//       }
+//       if (arr[i] %2 == 0 ){
+//         arr1 = arr.map(function(n){
+//                 return n ** 3
+//             })
+//       }
+//   }
+// console.log(arr1);
+
+// }
+// newArray(arr);
+
+
+// 3 chi masala
+
+// let arr = [10,25,15,17,19,20,45,9,27,30];
+// function karali (arr){
+//     let counter5 = 0;
+//     let counter3= 0;
+//     for (i = 0; i <= arr.length; i++){
+//             if (arr[i]% 5 == 0){
+//                 counter5 ++
+//             }
+//             if (arr[i] % 3 == 0){
+//                 counter3 ++
+//             }
+
+//     }
+//     if (counter5 > 0){
+//         console.log("5 ga karali son mavjud");
+//     }else{
+//         console.log("5 ga karli son mavjud emas");
+//     }
+//     if (counter3 > 0){
+//         console.log("3 ga karali son mavjud");
+//     }else{
+//         console.log("3 ga karli son mavjud emas");
+//     }
+//     localStorage.setItem(counter3, " ta 3 ga karali son mavjud");
+//     localStorage.setItem(counter5, "ta 5ga karli son mavjud")
+// }
+// karali(arr);
+
+// 6 chi masala
+
+
+// function matn(a){
+    // let n = 'salom bolalar'
+    // n = n.replace('r', 'p');
+    // console.log(n);
+// }
+
+//  8 masala 
+// let arr = [12,123,555,87.99,665,887,1,7,10,100,99]
+// function uchXonali(arr){
+//     let counter = 0;
+//     for (i = 0; i<=arr.length; i++){
+//        if (arr[i]>99 && arr[i]<1000){
+//         counter ++
+//        }
+//     }
+//     console.log(counter + "  ta 3 honali son mavjud");
+// }
+// uchXonali(arr);
+
+
+
